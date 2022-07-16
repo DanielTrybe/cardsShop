f8de0f3b4c12dddddb177b9e5929c2f58ac2ee3d
Daniel <darkdanielr@gmail.com>
2022-07-16 18:44:46 -0300

    funções de busca de cards

--- src/services/context/CardsList.tsx
+++ src/services/context/CardsList.tsx
@@ -4,8 +4,10 @@ import api from "services/api/api";
 
 interface CardsContextProps {
   cardsList: Array<any>;
+  getOneCard: (card: number) => void;
   search: string;
   setSearch: (value: string) => void;
+  card: any;
 }
 
 export const CardsContext = createContext({} as CardsContextProps);
@@ -14,24 +16,37 @@ const CardsProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   const [cardsList, setCardsList] = useState<any[]>([]);
+  const [card, setCard] = useState({} as any);
   const [search, setSearch] = useState("" as string);
 
+  const getOneCard = async (cardID: number) => {
+    try {
+      const response = await api.get(`cards/${cardID}`);
+      setCard(response.data);
+    } catch {
+      console.log("erro ao buscar carta");
+    }
+  };
+
+  const getCards = async () => {
+    try {
+      const response = await api.get("cards");
+      setCardsList(response.data);
+    } catch {
+      console.log("erro ao buscar cartas");
+    }
+  };
+
   useEffect(() => {
-    const getCards = async () => {
-      try {
-        const response = await api.get("cards");
-        setCardsList(response.data);
-      } catch {
-        console.log("erro ao buscar cartas");
-      }
-    };
     getCards();
   }, []);
 
   const values = {
     cardsList,
+    getOneCard,
     search,
     setSearch,
+    card,
   };
 
   return (
