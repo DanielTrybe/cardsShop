69999ae9ce461bc1f3c14bee3aaf205a6310bc7a
Daniel <darkdanielr@gmail.com>
2022-07-26 18:00:25 -0300

    bug corrigido da listagem

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -2,15 +2,12 @@ import { Container } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import Header from "./Header/Header";
 import Menus from "./Menus/Menu";
-import CardsProvider from "services/context/CardsList";
 
 export default function MainLayout() {
   return (
     <Container maxWidth="xl">
-      <CardsProvider>
-        <Header />
-        <Menus />
-      </CardsProvider>
+      <Header />
+      <Menus />
 
       <Outlet />
     </Container>
